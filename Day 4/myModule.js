813f1184137623a736f87f8be1fce9cde018ff2a
Abhinandan Singh <abhisakhhusingh@gmail.com>
2026-04-23 19:22:18 +0530

    Day 4

---
+++ Day 4/myModule.js
@@ -0,0 +1,12 @@
+
+
+age=56
+
+function add(a,b){
+  return a+b;
+}
+
+module.exports={
+add,
+age
+}
---
+++ Day 4/node.js
@@ -0,0 +1,9 @@
+const notes=require("./myModule.js")
+
+var fun=notes.add(4,8)
+
+console.log(fun);
+
+console.log("This is current file");
+console.log(notes.age);
+
